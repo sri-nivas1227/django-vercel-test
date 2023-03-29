06b6f6db1af0a3f65c781f77ff7152a80bfd069b
Sri-1227 <srinivasmekala1227@gmail.com>
2023-03-29 20:06:45 +0530

    static files collected

--- djtail/settings.py
+++ djtail/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6ejmwzrt=5jslbmvf62%^njbhkdjyed$kx3i+6b9$ab78u4aeq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app","127.0.0.1"]
+ALLOWED_HOSTS = [".vercel.app", "127.0.0.1"]
 
 
 # Application definition
@@ -122,7 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'app/static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -133,4 +138,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 NPM_BIN_PATH = r"C:\Users\srini\AppData\Roaming\npm\npm.cmd"
 TAILWIND_APP_NAME = 'theme'
 TAILWIND_CSS = 'css/tailwind.css'
-INTERNAL_IPS = ["127.0.0.1" ]
+INTERNAL_IPS = ["127.0.0.1"]
